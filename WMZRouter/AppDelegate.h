b1298b4eb61032515342e937849c6ad8e2fa13d0
“wmz1” <mingzhou.wu@xblgroup.com>
2018-11-08 09:43:02 +0800

    Initial Commit

---
+++ WMZRouter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WMZRouter
+//
+//  Created by wmz on 2018/11/8.
+//  Copyright © 2018年 wmz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
